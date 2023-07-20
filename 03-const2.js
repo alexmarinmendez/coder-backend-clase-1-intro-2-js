b302ee9ad5de6d27cefdc8683fc5517f3e56092f
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-19 19:53:43 -0500

    feat: add 03-const2.js

---
+++ 03-const2.js
@@ -0,0 +1,13 @@
+const persona = {
+    name: 'Alex',
+    apellido: 'Marin',
+    edad: 46
+}
+
+persona = {
+    name: 'Leandro',
+    apellido: 'Martinez',
+    edad: 35
+}
+
+console.log(persona)
